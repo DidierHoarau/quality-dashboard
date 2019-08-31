a553da6dcf6cf7066417cc067ac94f853f40bb39
Didier Hoarau <didier.hoarau.dev@gmail.com>
2019-08-31 19:17:05 +0800

    Change PWA colors (#14)

--- quality-dashboard-ui/vue.config.js
+++ quality-dashboard-ui/vue.config.js
@@ -1,6 +1,13 @@
+var manifestJSON = require("./public/manifest.json");
+
 module.exports = {
   devServer: {
     disableHostCheck: true,
   },
   publicPath: "/quality-dashboard/",
+  pwa: {
+    name: manifestJSON.short_name,
+    themeColor: manifestJSON.theme_color,
+    msTileColor: "#000000",
+  },
 };
